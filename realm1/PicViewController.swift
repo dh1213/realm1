b00d8906e065f6c3f019f6c588039a563243214e
usayuki <masapp.01a@gmail.com>
2020-09-29 14:00:37 +0900

    RealmのデータをPickerViewに表示

--- realm1/PicViewController.swift
+++ realm1/PicViewController.swift
@@ -13,47 +13,47 @@ import RealmSwift
 
 class PicViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     
-    @IBOutlet weak var pickview: UIPickerView!
+    @IBOutlet weak var pickerview: UIPickerView!
     @IBOutlet weak var sntksLbl: UILabel!
-        //Realmインスタンス
-        let realm = try! Realm()
-        var ret: Results <TodoModel2>!
+    //Realmインスタンス
+    let realm = try! Realm()
+    var ret: Results <TodoModel2>!
 
-        //PickerViewの列数
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    //PickerViewの列数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
-        }
+    }
 
-        //PIckerViewの行数,リスト数
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    //PIckerViewの行数,リスト数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return ret.count
-        }
+    }
 
-        // ドラムロールの各タイトル
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return nil
-        }
+    // ドラムロールの各タイトル
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ret[row].todo2
+    }
 
-        //Pickeriewから選択された時のイベント
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.sntksLbl.text = ""
-        }
-           
-        // 画面ロード時
-        override func viewDidLoad() {
+    //Pickeriewから選択された時のイベント
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.sntksLbl.text = ret[row].todo2
+    }
+
+    // 画面ロード時
+    override func viewDidLoad() {
         super.viewDidLoad()
 
-            // Delegate設定
-           pickerView.delegate = self
-           pickerView.dataSource = self
-            
+        // Delegate設定
+        pickerview.delegate = self
+        pickerview.dataSource = self
+
         sntksLbl.text = "初期値"
 
         //Realmインスタンス
         let realm = try! Realm()
-            
+
         //事業所マスタより取得
         ret = realm.objects(TodoModel2.self)
 
-        }
+    }
 }
